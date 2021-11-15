945dc2cf7e76bd3e5dc14c7e095f2a125e75bcd9
Jasper M-W <jasper@ngita.co.nz>
2021-11-15 21:09:21 +1300

    Very start of table component

---
+++ src/lib/components/PeopleTable.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import type { Attendee } from '$lib/attendee';
+	export let peopleList: Attendee[] = [];
+</script>
+<table>
+	<tr>
+		<th>Name</th>
+		<th>ID</th>
+	</tr>
+	{#each peopleList as person}
+		<tr>
+			<th>{person.first_name} {person.last_name}</th>
+			<th>{person.id}</th>
+		</tr>
+	{/each}
+</table>
+
+<style lang="scss">
+	@use '../styles/vars.scss' as *;
+	table {
+		background-color: $background-intermediate-light;
+	}
+</style>
---
+++ src/stories/PeopleTable.stories.svelte
@@ -0,0 +1,20 @@
+<script>
+	import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+
+	import PeopleTable from '../lib/components/PeopleTable.svelte';
+    import {generateLatestCheckIns} from "../lib/mocks/attendeeMock";
+	import faker from 'faker';
+	faker.seed(1);
+</script>
+
+<Meta
+	title="People Table"
+	component={PeopleTable}
+    argTypes={{ peopleCount: {control:'number'} }}
+/>
+
+<Template let:args>
+	<PeopleTable peopleList={generateLatestCheckIns(args.peopleCount)}/>
+</Template>
+
+<Story name="default" args={{peopleCount: 10}} />
