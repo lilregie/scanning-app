c8c0338cda92be085c1f36c628419ad22c022d55
Zac M-W <z.millerwaugh@auckland.ac.nz>
2021-11-20 15:00:27 +1300

    Initial implementation of scanner

---
+++ src/lib/components/Scanner.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import QR from 'modern-svelte-qr-scanner';
+
+	let previewWidth;
+	let mediaErrorMessage = '';
+
+	function onQRScan(event: CustomEvent) {
+		alert(event.detail.qrContent);
+	}
+</script>
+
+<div class="qr-container">
+	<div class="qr-wrapper" bind:clientWidth={previewWidth}>
+		<QR
+			on:scan={onQRScan}
+			previewWidth_px={previewWidth}
+			previewHeight_px={previewWidth}
+			bind:mediaErrorMessage
+		>
+			<div slot="loading" class="loading">
+				<span>Loading Animation, but text</span>
+			</div>
+			<div slot="failedToInitialize" class="failed-to-initialize">
+				Failed to initialize camera.<br />
+				Error: {mediaErrorMessage}
+			</div>
+		</QR>
+	</div>
+</div>
