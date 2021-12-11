a81f163a5a1b74fc420da53a3bb70cf61ebbdec2
Jasper Miller-Waugh <contact@jmw.nz>
2021-12-11 15:44:51 +1300

    Added Local Storage Prefix

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -4,19 +4,22 @@ import type { Event } from '$lib/event';
 import type { Attendee } from './attendee';
 import { getAttendeesList } from './api';
 
+let LOCAL_STORAGE_VERSION = 1;
+
 /** Makes a store persistent in local storage
  * @param  {[Writable]} store Writable store to be made persistent
  * @param  {[string]} key Key to be used in Local Storage
  */
 function useLocalStorage<T>(store: Writable<T>, key: string) {
+	let localStorageKey = `lilregie_v${LOCAL_STORAGE_VERSION}_${key}`;
 	if (typeof localStorage !== 'undefined') {
-		const json = localStorage.getItem(key);
+		const json = localStorage.getItem(localStorageKey);
 		if (json) {
 			store.set(JSON.parse(json));
 		}
 
 		store.subscribe((current) => {
-			localStorage.setItem(key, JSON.stringify(current));
+			localStorage.setItem(localStorageKey, JSON.stringify(current));
 		});
 	}
 }
