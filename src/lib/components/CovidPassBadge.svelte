9206e911ade724b4274ea4b47a60b5e63bacaf8f
ur5us <juhah@web.de>
2022-01-21 11:51:27 +1300

    Fix indentation

--- src/lib/components/CovidPassBadge.svelte
+++ src/lib/components/CovidPassBadge.svelte
@@ -1,61 +1,61 @@
 <script lang="ts">
-    import SuccessTick from "$lib/components/SuccessTick.svelte";
-    import dayjs from 'dayjs';
-    import relativeTime from 'dayjs/plugin/relativeTime.js';
+	import SuccessTick from "$lib/components/SuccessTick.svelte";
+	import dayjs from 'dayjs';
+	import relativeTime from 'dayjs/plugin/relativeTime.js';
 
-    dayjs.extend(relativeTime);
+	dayjs.extend(relativeTime);
 
-    export let name: string;
-    export let dob: string;
+	export let name: string;
+	export let dob: string;
 </script>
 <div class="container">
-    <div class="tick-wrapper">
-        <SuccessTick colour="green"/>
-
-    </div>
-    <div>
-        <div class="header">
-            COVID Pass Vaild
-        </div>
-        <div class="details">
-            {name}
-            <br/>
-            Age: <span title={dob}>{dayjs(dob).toNow(true)}</span>
-        </div>
-    </div>
+	<div class="tick-wrapper">
+		<SuccessTick colour="green"/>
+	</div>
+	<div>
+		<div class="header">
+			COVID Pass Vaild
+		</div>
+		<div class="details">
+			{name}
+			<br/>
+			Age: <span title={dob}>{dayjs(dob).toNow(true)}</span>
+		</div>
+	</div>
 </div>
 <style lang="scss">
 	@use '../styles/vars.scss' as *;
-    .container {
-        background-color: $background-intermediate-light;
-        display: flex;
-        & > * {
-            margin: 0.5rem 1rem;
-        }
-        .tick-wrapper {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            :global(svg) {
-                height: 40px;
-            }
-        }
-  
-    }
-    .header {
-        font-size: 1.5rem;
-        font-weight: 600;
-        
-    }
-    @media (max-width: 768px) {
-        .header {
-            font-size: 1rem;
+	.container {
+		background-color: $background-intermediate-light;
+		display: flex;
 
-        }
-        .details {
-            font-size: 0.8rem;
+		& > * {
+			margin: 0.5rem 1rem;
+		}
 
-        }
-    }
-    
+		.tick-wrapper {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+
+			:global(svg) {
+				height: 40px;
+			}
+		}
+	}
+
+	.header {
+		font-size: 1.5rem;
+		font-weight: 600;
+	}
+
+	@media (max-width: 768px) {
+		.header {
+			font-size: 1rem;
+		}
+
+		.details {
+			font-size: 0.8rem;
+		}
+	}
 </style>
