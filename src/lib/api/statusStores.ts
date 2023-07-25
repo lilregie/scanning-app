f8a67018838c427918b35c9c83e33c10ef52ce6e
ur5us <juhah@web.de>
2023-07-26 11:32:09 +1200

    Improve code formatting

--- src/lib/api/statusStores.ts
+++ src/lib/api/statusStores.ts
@@ -1,9 +1,9 @@
 import { writable } from "svelte/store";
 import type { Writable } from 'svelte/store';
 
-
 export const errorAPICallbacks: Writable<(() => void)[]> = writable([]);
-export const apiTimers: Writable<Map<string,number>> = writable(new Map());
+
+export const apiTimers: Writable<Map<string, number>> = writable(new Map());
 
 export const csrfAPIState: Writable<string | null> = writable(null);
 
