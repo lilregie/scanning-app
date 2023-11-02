f2bc86e6b1c7049476be6b2f13004e9472c5ceba
ur5us <juhah@web.de>
2023-11-02 13:44:56 +1300

    Add Sentry error tracking

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,3 +1,17 @@
 import { initializeAPI } from '$lib/api/api'
+import * as Sentry from '@sentry/sveltekit';
+import { PUBLIC_SENTRY_DSN } from '$env/static/public'
 
 initializeAPI()
+
+// If you don't want to use Session Replay, remove the `Replay` integration,
+// `replaysSessionSampleRate` and `replaysOnErrorSampleRate` options.
+Sentry.init({
+    dsn: PUBLIC_SENTRY_DSN,
+    tracesSampleRate: 1,
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1,
+    integrations: [new Sentry.Replay()]
+})
+
+export const handleError = Sentry.handleErrorWithSentry();
--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import { sentrySvelteKit } from "@sentry/sveltekit";
 import { sveltekit } from '@sveltejs/kit/vite';
 import svg from '@poppanator/sveltekit-svg';
 import childProcess from 'node:child_process';
@@ -12,14 +13,22 @@ try {
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [
+		sentrySvelteKit({
+			sourceMapsUploadOptions: {
+				org: "lilregie",
+				project: "checkin-app"
+			}
+		}),
 		sveltekit(),
 		svg({
 			type: 'url'
 		})
 	],
+
 	optimizeDeps: {
 		include: ['events', 'uuid', 'visibilityjs', 'stampit', 'lodash', 'dayjs']
 	},
+
 	define: {
 		__COMMIT_HASH__: JSON.stringify(lastCommitHash)
 	}
