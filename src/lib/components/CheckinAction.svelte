d645604aed0ce4f0c888e7cc49b2a0650494e54d
ur5us <juhah@web.de>
2024-03-28 11:40:17 +1300

    Display cancelled status in more places
    Previously, we’d only show a cancellation related error messages upon
    trying to scan + check-in. Now, it’ll also show the cancellation status
    when showing the attendee details.
    
    Fixes LIL-337

--- src/lib/components/CheckinAction.svelte
+++ src/lib/components/CheckinAction.svelte
@@ -9,10 +9,11 @@
 	export let attendee: Attendee
 	export let variant: "inline" | "block" = "inline"
 
-	let disabled: boolean = false
+	let disabled: boolean = !!attendee.cancelled_at
+	let loading: boolean = false
 
 	async function updateCheckinStatus() {
-		disabled = true
+		disabled = loading = true
 
 		const data = new FormData(this)
 		const response = await fetch(this.action, {
@@ -31,7 +32,11 @@
 			applyAction({ status: response.status, type: "failure", data: await response.json() })
 		}
 
-		disabled = false
+		disabled = loading = false
+	}
+
+	function isCancelled() {
+		return !!attendee.cancelled_at
 	}
 </script>
 
@@ -51,14 +56,17 @@
 	<button
 		class="btn-check-in flex-shrink-0"
 		class:isDone={ !!attendance.checked_in_at }
+		class:isCancelled={ isCancelled() }
 		class:flex-grow={ variant === "block" }
 		class:justify-center={ variant === "block" }
 		{ disabled }
 	>
-		{#if disabled}
+		{#if loading}
 			<svg width="16" height="16" viewBox="0 0 17 17" xmlns="http://www.w3.org/2000/svg" class="animate-spin m-auto">
 				<path d="M10 2C10 2.84375 9.3125 3.5 8.5 3.5C7.65625 3.5 7 2.84375 7 2C7 1.1875 7.65625 0.5 8.5 0.5C9.3125 0.5 10 1.1875 10 2ZM10 15C10 15.8438 9.3125 16.5 8.5 16.5C7.65625 16.5 7 15.8438 7 15C7 14.1875 7.65625 13.5 8.5 13.5C9.3125 13.5 10 14.1875 10 15ZM0.5 8.5C0.5 7.6875 1.15625 7 2 7C2.8125 7 3.5 7.6875 3.5 8.5C3.5 9.34375 2.8125 10 2 10C1.15625 10 0.5 9.34375 0.5 8.5ZM16.5 8.5C16.5 9.34375 15.8125 10 15 10C14.1562 10 13.5 9.34375 13.5 8.5C13.5 7.6875 14.1562 7 15 7C15.8125 7 16.5 7.6875 16.5 8.5ZM2.8125 14.1562C2.25 13.5938 2.25 12.625 2.8125 12.0625C3.40625 11.4688 4.375 11.4688 4.9375 12.0625C5.53125 12.625 5.53125 13.5938 4.9375 14.1562C4.375 14.75 3.40625 14.75 2.8125 14.1562ZM4.9375 4.96875C4.375 5.5625 3.40625 5.5625 2.8125 4.96875C2.25 4.40625 2.25 3.4375 2.8125 2.84375C3.40625 2.28125 4.375 2.28125 4.9375 2.84375C5.53125 3.4375 5.53125 4.40625 4.9375 4.96875ZM12.0312 12.0625C12.5938 11.4688 13.5625 11.4688 14.1562 12.0625C14.7188 12.625 14.7188 13.5938 14.1562 14.1562C13.5625 14.75 12.5938 14.75 12.0312 14.1562C11.4375 13.5938 11.4375 12.625 12.0312 12.0625Z"/>
 			</svg>
+		{:else if isCancelled()}
+			Cancelled
 		{:else if !!attendance.checked_in_at}
 			Checked-in
 		{:else}
@@ -101,6 +109,12 @@
 				background: #FFFFFF;
 				color: #3DC393;
 			}
+		}
+
+		&.isCancelled {
+			border-color: map-get($theme-colors, 'warning');
+			background: map-get($theme-colors, 'warning');
+			color: #FFFFFF;
 		}
 	}
 </style>
